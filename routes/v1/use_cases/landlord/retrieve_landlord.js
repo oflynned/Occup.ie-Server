d979c6999e746d4e2bd5039b5f3d67d4fe2606ca
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-01 23:53:43 +0000

    updated use cases to include creation, retrieval and refactor of applications and landlord retrieval

--- routes/v1/use_cases/landlord/retrieve_landlord.js
+++ routes/v1/use_cases/landlord/retrieve_landlord.js
@@ -30,21 +30,6 @@ function deleteLandlord(db, id) {
     })
 }
 
-function getLandlordParams(user, updatedLandlord) {
-    user["email"] = updatedUser["email"] === undefined ? user["email"] : updatedUser["email"];
-    user["forename"] = updatedUser["forename"] === undefined ? user["forename"] : updatedUser["forename"];
-    user["surname"] = updatedUser["surname"] === undefined ? user["surname"] : updatedUser["surname"];
-    user["facebook_id"] = updatedUser["facebook_id"] === undefined ? user["facebook_id"] : updatedUser["facebook_id"];
-    user["facebook_token"] = updatedUser["facebook_token"] === undefined ? user["facebook_token"] : updatedUser["facebook_token"];
-    user["profile_picture"] = updatedUser["profile_picture"] === undefined ? user["profile_picture"] : updatedUser["profile_picture"];
-    user["age"] = updatedUser["age"] === undefined ? user["age"] : updatedUser["age"];
-    user["sex"] = updatedUser["sex"] === undefined ? user["sex"] : updatedUser["sex"];
-    user["profession"] = updatedUser["profession"] === undefined ? user["profession"] : updatedUser["profession"];
-
-    validatePayload(user);
-    return user;
-}
-
 module.exports = {
     getLandlords: getLandlords,
     modifyLandlord: modifyLandlord,
