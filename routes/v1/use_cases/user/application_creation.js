3209fb863df2d42a0ae1e3b2287d0f2de1357def
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-03 16:26:25 +0000

    refactor of files with better naming

---
+++ routes/v1/use_cases/user/application_creation.js
@@ -0,0 +1,37 @@
+let ObjectId = require('mongodb').ObjectID;
+const collection = require("../../common/collections").applications;
+
+function getApplications(db, filter={}) {
+    return new Promise((res, rej) => {
+        db.get(collection)
+            .find(filter)
+            .then((records) => res(records))
+            .catch((err) => rej(err))
+    })
+}
+
+function modifyApplication(db, id, data) {
+    return new Promise((res, rej) => {
+        db.get(collection)
+            .update({_id: ObjectId(id)}, {"$set": data})
+            .then(() => {
+                res(data)
+            })
+            .catch((err) => rej(err));
+    })
+}
+
+function deleteApplication(db, id) {
+    return new Promise((res, rej) => {
+        db.get(collection)
+            .remove({_id: ObjectId(id)})
+            .then((record) => res(record))
+            .catch((err) => rej(err))
+    })
+}
+
+module.exports = {
+    getApplications: getApplications,
+    modifyApplication: modifyApplication,
+    deleteApplication: deleteApplication
+};
