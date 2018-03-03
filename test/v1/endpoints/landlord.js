9122f5802dab1b6a88c2c855b72f1d3dcfd80c37
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-03 19:21:29 +0000

    fixed landlord regression endpoint issue

--- test/v1/endpoints/landlord.js
+++ test/v1/endpoints/landlord.js
@@ -14,36 +14,45 @@ const creationUseCase = require("../../../routes/v1/use_cases/landlord/landlord_
 const retrievalUseCase = require("../../../routes/v1/use_cases/landlord/landlord_account_retrieval");
 
 function dropDb() {
-    db.get(collection).drop();
+    return db.get(collection).drop()
 }
 
 function seedDb() {
-    let landlord1 = model.generate("John", "Smith", "john.smith@test.com", "0");
-    let landlord2 = model.generate("Emma", "Sheeran", "emma.sheeran@test.com", "1");
-    let landlord3 = model.generate("Edmond", "O'Flynn", "edmond.oflynn@test.com", "2");
+    const landlord1 = model.generate("John", "Smith", "john.smith@test.com", "0");
+    const landlord2 = model.generate("Emma", "Sheeran", "emma.sheeran@test.com", "1");
+    const landlord3 = model.generate("Edmond", "O'Flynn", "edmond.oflynn@test.com", "2");
 
-    creationUseCase.createAccount(db, collection, landlord1);
-    creationUseCase.createAccount(db, collection, landlord2);
-    creationUseCase.createAccount(db, collection, landlord3);
+    return Promise.all([
+        creationUseCase.createAccount(db, collection, landlord1),
+        creationUseCase.createAccount(db, collection, landlord2),
+        creationUseCase.createAccount(db, collection, landlord3)
+    ]);
 }
 
 describe("api landlord account creation", () => {
-    before(() => {
-        chai.use(chaiHttp);
-        dropDb();
-        seedDb();
+    beforeEach((done) => {
+        dropDb()
+            .then(() => seedDb())
+            .then(() => {
+                chai.use(chaiHttp);
+                done()
+            })
+            .catch((err) => done(err));
     });
 
-    after(() => dropDb());
+    afterEach((done) => {
+        dropDb().then(() => done()).catch((err) => done(err));
+    });
 
     it("requesting a landlord by id should return 200", (done) => {
         chai.request(app)
             .get("/api/v1/landlord")
             .then((res) => {
                 assert.equal(res.status, 200);
-                console.log(res.body);
+                assert.equal(res.body.length, 3);
                 done();
             })
             .catch((err) => done(err))
+
     });
 });
