18bf5eec5a8e2f886e054d016df3168c70904750
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-05 22:22:33 +0000

    updated listing model to be more simplified, will be refactored to multiple models in the future with inheritance'

--- routes/v1/models/listing.js
+++ routes/v1/models/listing.js
@@ -2,6 +2,7 @@ const Joi = require("joi");
 
 const schema = Joi.object().keys({
     type: Joi.string().valid("rent", "house_share").required(),
+    landlord_uuid: Joi.string().required(),
     address: {
         house_number: Joi.string().required(),
         street: Joi.string().required(),
@@ -11,37 +12,29 @@ const schema = Joi.object().keys({
         eircode: Joi.string().required()
     },
     details: {
-        dwelling: Joi.valid("studio", "apartment", "house", "room").required(),
+        dwelling: Joi.string().valid("studio", "apartment", "house").required(),
         description: Joi.string().required(),
-        lat: Joi.number().required(),
-        lng: Joi.number().required(),
         stay_caveats: Joi.string(),
-        move_in_date: Joi.date().required(),
-        lease_length: Joi.number().required(),
-        min_target_age_category: Joi.number().min(18).required(),
-        max_target_age_category: Joi.number().min(18).required(),
-        target_profession: Joi.string().required(),
-        target_sex: Joi.string().valid("male", "female", "other", "couple").required()
+        lease_length_months: Joi.number().required(),
+        min_target_age: Joi.number().min(18).required(),
+        max_target_age: Joi.number().min(18).required(),
+        target_sex: Joi.string().valid("male", "female", "other", "couple").required(),
+        target_profession: Joi.string().valid("student", "professional").required()
     },
-    bathrooms: Joi.array().items(Joi.object({
-        size: Joi.string().valid("ensuite", "separate").required()
-    })),
-    bedrooms: Joi.array().items(Joi.object({
-        size: Joi.string().valid("single", "double", "twin").required(),
-        monthly_cost: Joi.number().required(),
-        deposit: Joi.number().required()
-    })),
+
+    bedrooms: Joi.array().items(Joi.string().valid("single", "double", "twin")).required(),
+    bathrooms: Joi.array().items(Joi.string().valid("ensuite", "shared").required()),
     images: Joi.array().items(Joi.string().required()),
+
     facilities: {
-        washing_machine: Joi.boolean(),
-        dryer: Joi.boolean(),
-        parking: Joi.boolean(),
-        furnished: Joi.boolean(),
-        wifi: Joi.boolean(),
-        central_heating: Joi.boolean()
+        washing_machine: Joi.boolean().required(),
+        dryer: Joi.boolean().required(),
+        parking: Joi.boolean().required(),
+        wifi: Joi.boolean().required(),
+        central_heating: Joi.boolean().required(),
+        garden: Joi.boolean().required()
     },
     listing: {
-        landlord_uuid: Joi.string().required(),
         created: Joi.date().required(),
         updated: Joi.date(),
         expires: Joi.date().required(),
@@ -49,15 +42,93 @@ const schema = Joi.object().keys({
         status: Joi.string().required(),
         owner_occupied: Joi.boolean().required(),
         furnished: Joi.boolean().required(),
-        bill_estimate: Joi.string(),
-        ber: Joi.string()
+        ber: Joi.string().valid(
+            "A1", "A2", "A3",
+            "B1", "B2", "B3",
+            "C1", "C2", "C3",
+            "D1", "D2",
+            "E1", "E2",
+            "F",
+            "G"
+        )
     }
 });
 
+function generateAddress(houseNumber, street, area, city, county, eircode) {
+    return {
+        house_number: houseNumber,
+        street: street,
+        area: area,
+        city: city,
+        county: county,
+        eircode: eircode
+    }
+}
+
+function generateDetails(dwelling, description, stayCaveats, leaseLengthMonths, minAge, maxAge, targetSex, targetProfession) {
+    return {
+        dwelling: dwelling,
+        description: description,
+        stayCaveats: stayCaveats,
+        lease_length_months: leaseLengthMonths,
+        min_target_age: minAge,
+        max_target_age: maxAge,
+        target_sex: targetSex,
+        target_profession: targetProfession,
+    }
+}
+
+function generateFacilities(hasWashingMachine, hasDryer, hasParking, hasWifi, hasCentralHeating, hasGarden) {
+    return {
+        washing_machine: hasWashingMachine,
+        dryer: hasDryer,
+        parking: hasParking,
+        wifi: hasWifi,
+        central_heating: hasCentralHeating,
+        garden: hasGarden
+    }
+}
+
+function generateListing(plan, isOwnerOccupied, isFurnished, ber) {
+    const creation = new Date();
+    let expiry = new Date();
+    expiry.setDate(creation.getDate() + 21);
+
+    return {
+        created: creation,
+        updated: creation,
+        expires: expiry,
+        plan: plan,
+        status: "active",
+        owner_occupied: isOwnerOccupied,
+        furnished: isFurnished,
+        ber: ber
+    }
+}
+
+function generate(type, landlordUuid, address, details, bathrooms, bedrooms, facilities, listing) {
+    return {
+        type: type,
+        landlord_uuid: String(landlordUuid),
+        address: address,
+        details: details,
+        bathrooms: bathrooms,
+        bedrooms: bedrooms,
+        images: ["http://f1.thejournal.ie/media/2013/08/james-larkin-2-337x500.jpg"],
+        facilities: facilities,
+        listing: listing
+    }
+}
+
 function validate(o) {
-    return Joi.validate(o, schema);
+    return Joi.validate(o, schema, {allowUnknown: true})
 }
 
 module.exports = {
+    generateAddress: generateAddress,
+    generateDetails: generateDetails,
+    generateFacilities: generateFacilities,
+    generateListing: generateListing,
+    generate: generate,
     validate: validate
 };
