a29e76a2ba08109dc914ebfd7aa5ab8e0b5bab94
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-10 13:18:19 +0000

    s3 changed to reflect the server direct usage of db backups periodically

--- routes/v1/common/s3.js
+++ routes/v1/common/s3.js
@@ -1,7 +1,7 @@
 const s3Config = require("../../../config/s3");
-
 let AWS = require("aws-sdk");
-AWS.config.update(s3Config.awsCredentials);
-let s3Bucket = new AWS.S3({params: s3Config.imageBucketName});
 
-module.exports = s3Bucket;
+module.exports = () => {
+    AWS.config.update(s3Config.awsCredentials);
+    return new AWS.S3({params: s3Config.dbBucketName});
+};
