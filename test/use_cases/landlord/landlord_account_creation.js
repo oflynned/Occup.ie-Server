3ecce19c4fd63edfb0619efd8a6449b6092a402b
Edmond O' Flynn <oflynned@tcd.ie>
2018-05-19 19:26:15 +0100

    updated landlord account creation

--- test/use_cases/landlord/landlord_account_creation.js
+++ test/use_cases/landlord/landlord_account_creation.js
@@ -7,6 +7,7 @@ const collection = require("../../../config/collections").development.landlords;
 let model = require("../../../models/landlord");
 let useCase = require("../../../models/use_cases/landlord/landlord_account_creation");
 const birthday = new Date(1960, 1, 1);
+let landlord = model.generate("John", "Smith", birthday, "male", "+353 86 123 4567");
 
 function dropDb() {
     db.get(collection).drop();
@@ -17,29 +18,23 @@ describe("landlord account creation tests", () => {
     afterEach(() => dropDb());
 
     it("should follow landlord schema for generate account", (done) => {
-        let landlord = model.generate("John", "Smith", birthday, "john.smith@test.com", "+353 86 123 4567");
+        let landlord = model.generate("John", "Smith", birthday, "male", "+353 86 123 4567");
         useCase.validatePayload(landlord)
-            .then(() => useCase.createAccount(db, collection, landlord))
-            .then((record) => {
-                assert.equal(record, landlord);
-                done();
-            })
-            .catch((err) => done(err.message));
+            .then(() => done())
+            .catch((err) => done(err));
     });
 
     it("should throw an error on account missing params", (done) => {
-        let landlord = model.generate("John", "Smith", birthday, "john.smith@test.com", "+353 86 123 4567");
+        let landlord = model.generate("John", "Smith", birthday, "male", "+353 86 123 4567");
         delete landlord["details"]["forename"];
-
         useCase.validatePayload(landlord)
             .then(() => done(new Error("incorrectly validated a wrong payload")))
             .catch(() => done());
     });
 
     it("should discard junk params provided to landlord account creation", (done) => {
-        let landlord = model.generate("John", "Smith", birthday, "john.smith@test.com", "+353 86 123 4567");
+        let landlord = model.generate("John", "Smith", birthday, "male", "+353 86 123 4567");
         landlord["parameter"] = "junk";
-
         let result = model.validateModel(landlord);
         assert.equal(result["error"] !== null, true);
 
